dc8eb5904b21d729eb773b5d1269fe14b938973a
Sakshi Mishra <180986964+Sakshi0256@users.noreply.github.com>
2026-07-06 23:02:38 +0530

    Re-enable role authorization for superadmin stats route

--- src/routes/superAdminRoute.js
+++ src/routes/superAdminRoute.js
@@ -5,7 +5,7 @@ const { getSuperAdminStats } = require('../controllers/superAdminController');
 const { protect, authorizeRoles } = require('../middleware/auth');
 
 // Ab yahan 'router' kaam karega
-// router.get('/stats', protect, authorizeRoles('superadmin'), getSuperAdminStats);
-router.get('/stats', protect, getSuperAdminStats);
+router.get('/stats', protect, authorizeRoles('superadmin'), getSuperAdminStats);
+// router.get('/stats', protect, getSuperAdminStats);
 
 module.exports = router;
